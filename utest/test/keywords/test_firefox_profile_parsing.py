0facfed6b68a23aca5d6db58dddcd7574c3564c7
Ed Manlove <emanlove@verizon.net>
2024-01-23 15:07:50 -0500

    Update for selenium v4.17.2 handling of firefox profile

--- utest/test/keywords/test_firefox_profile_parsing.py
+++ utest/test/keywords/test_firefox_profile_parsing.py
@@ -56,8 +56,8 @@ class FireFoxProfileParsingTests(unittest.TestCase):
 
     def _parse_result(self, result):
         to_str = ""
-        if "key1" in result.default_preferences:
-            to_str = f"{to_str} key1 {result.default_preferences['key1']}"
-        if "key2" in result.default_preferences:
-            to_str = f"{to_str} key2 {result.default_preferences['key2']}"
+        if "key1" in result._desired_preferences:
+            to_str = f"{to_str} key1 {result._desired_preferences['key1']}"
+        if "key2" in result._desired_preferences:
+            to_str = f"{to_str} key2 {result._desired_preferences['key2']}"
         self.results.append(to_str)
